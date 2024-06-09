e5f4cde03344716345ff7a4b8bd944afc0afc148
andriipolishko <andrii.polishko@aisdr.com>
2024-06-09 12:46:18 +0300

    Revert "add initial project"
    This reverts commit 7cb5c14d6df83f61c30bab6b625564cced40f3fb.

--- scripts/create-mysql-container.sh
+++
@@ -1,7 +0,0 @@
-docker network create mynetwork
-
-docker run --name mysqlContainer --network mynetwork -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -v $(pwd)/mysqlSetup/init-db.sql:/docker-entrypoint-initdb.d/init-db.sql -d mysql
-
-echo "Waiting 10sec for mysql to start..."
-
-sleep 10
--- scripts/create-server-image.sh
+++
@@ -1,1 +0,0 @@
-docker build -t genesis-test-proj-image .
