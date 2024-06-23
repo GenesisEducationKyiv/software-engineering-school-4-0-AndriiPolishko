e9c450ee5a1855687c2b970fc6d43b1138bd65e7
andriipolishko <andrii.polishko@aisdr.com>
2024-06-23 21:48:13 +0300

    fix tests

--- src/modules/exchange/exchange.service.test.ts
+++ src/modules/exchange/exchange.service.test.ts
@@ -2,22 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { HttpException } from '@nestjs/common';
 import axios from 'axios';
+
 import { MailerService } from '@nestjs-modules/mailer';
 import { Repository } from 'typeorm';
 import { ExchangeService } from './exchange.service';
 import { Subscriber } from '../../db/entities/subscriber.entity';
+import { SubscriptionService } from '../subscription/subscription.service';
 
 jest.mock('axios');
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
   let subscriberRepository: Repository<Subscriber>;
-  // let mailService: MailerService;
+  let subscriptionService: SubscriptionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ExchangeService,
+        SubscriptionService,
         {
           provide: getRepositoryToken(Subscriber),
           useClass: Repository,
@@ -32,6 +35,7 @@ describe('ExchangeService', () => {
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    subscriptionService = module.get<SubscriptionService>(SubscriptionService);
     subscriberRepository = module.get<Repository<Subscriber>>(getRepositoryToken(Subscriber));
   });
 
@@ -62,7 +66,7 @@ describe('ExchangeService', () => {
       jest.spyOn(subscriberRepository, 'findOne').mockResolvedValue(null);
       jest.spyOn(subscriberRepository, 'save').mockResolvedValue({ email } as Subscriber);
 
-      const subscriber = await service.subscribe(email);
+      const subscriber = await subscriptionService.subscribe(email);
       expect(subscriber.email).toEqual(email);
     });
 
@@ -70,7 +74,7 @@ describe('ExchangeService', () => {
       const email = 'test@example.com';
       jest.spyOn(subscriberRepository, 'findOne').mockResolvedValue({ email } as Subscriber);
 
-      await expect(service.subscribe(email)).rejects.toThrow(HttpException);
+      await expect(subscriptionService.subscribe(email)).rejects.toThrow(HttpException);
     });
   });
 });
